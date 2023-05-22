4fd9c5ce6060a484883cfa0da447ae4b2a3a3b42
Zhambulova Tabigat <zhambulovat@gmail.com>
2023-05-22 18:57:11 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const telegramApiToken = process.env.TELEGRAM_API_TOKEN; // Use the telegramApiToken variable to authenticate with the Telegram Bot API
+const {Telegraf} = require('telegraf');
+
+const bot = new Telegraf(telegramApiToken);
+
+bot.start((ctx) => {
+    //handle the /start command
+    ctx.reply('Welcome to the Healthy Recipe Bot! How can I assist you?');
+});
+
+//handle the /recipe command
+bot.launch();
